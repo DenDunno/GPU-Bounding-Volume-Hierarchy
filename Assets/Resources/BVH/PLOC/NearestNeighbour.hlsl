b9eb7ce5f97ebb45af9dc25f6aa381fcfffcc83a
Denis Bondar <60221683+DenDunno@users.noreply.github.com>
2024-11-09 17:25:29 -0800

    EncodeOffsetIntoLowerBits rewrite

--- Assets/Resources/BVH/PLOC/NearestNeighbour.hlsl
+++ Assets/Resources/BVH/PLOC/NearestNeighbour.hlsl
@@ -6,20 +6,25 @@
 #define UINT_MAX_VALUE -1
 #define SEARCH_RADIUS_SHIFT 4
 #define SEARCH_RADIUS 1 << SEARCH_RADIUS_SHIFT
-#define ENCODE_MASK (1 << (SEARCH_RADIUS_SHIFT + 1) - 1)
+#define ENCODE_MASK ~(1 << (SEARCH_RADIUS_SHIFT + 1) - 1)
 groupshared uint Neighbours[THREADS];
 
-
-uint EncodeRelativeOffset(const uint id, const uint neighbor)
+uint EncodeOffsetIntoLowerBits(const uint id, const uint neighbor)
 {
-    const uint uOffset = neighbor - id - 1;
-    return uOffset << 1;
+    const int signedOffset = neighbor - id;
+    const uint signLastLowerBit = signedOffset >> 31;
+    const uint valueUpperBits = (abs(signedOffset) - 1) << 1;
+    return valueUpperBits | signLastLowerBit;
+}
+
+void DecodeOffset()
+{
 }
 
 void InitializeNeighbours(uint threadId)
 {
     Neighbours[threadId] = UINT_MAX_VALUE;
-    GroupMemoryBarrierWithGroupSync();   
+    GroupMemoryBarrierWithGroupSync();
 }
 
 float ComputeDistanceBetweenNodes(uint first, uint second)
@@ -27,15 +32,20 @@ float ComputeDistanceBetweenNodes(uint first, uint second)
     return Nodes[first].Box.Union(Nodes[second].Box).ComputeSurfaceArea();
 }
 
-void FindNearestNeighbour(uint threadId)
+uint FindNearestNeighbour(uint id)
 {
-    InitializeNeighbours(threadId);
+    InitializeNeighbours(id);
     uint minAreaIndex = UINT_MAX_VALUE;
 
     [unroll(SEARCH_RADIUS)]
     for (int i = 1; i <= SEARCH_RADIUS; i++)
     {
-        float area = ComputeDistanceBetweenNodes(threadId, threadId + 1);
-        uint areaInteger = asuint(area);
+        float area = ComputeDistanceBetweenNodes(id, id + 1);
+        uint positiveAreaInteger = asuint(area) << 1;
+        uint areaUpperBits = positiveAreaInteger & ENCODE_MASK;
+        uint encode0 = areaUpperBits | EncodeOffsetIntoLowerBits(id, id + i);
+        uint encode1 = areaUpperBits | EncodeOffsetIntoLowerBits(id + i, id);
     }
+
+    return Neighbours[id];
 }
